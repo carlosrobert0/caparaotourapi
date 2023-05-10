e5a48de2544d866d15b8e5cc87415bd8aa93c8a7
carlosrobert0 <carlosroberto15755@gmail.com>
2023-05-09 23:53:30 -0300

    include city and place in return find many depositions

--- src/modules/depositions/useCases/findAllDepositions/findAllDepositionsUseCase.ts
+++ src/modules/depositions/useCases/findAllDepositions/findAllDepositionsUseCase.ts
@@ -2,7 +2,16 @@ import { prisma } from "../../../../database/prismaClient";
 
 export class FindAllDepositionsUseCase {
   async execute() {
-    const depositions = await prisma.depositions.findMany({})
+    const depositions = await prisma.depositions.findMany({
+      include: {
+        city: true,
+        place: {
+          include: {
+            category: true
+          },
+        }
+      }
+    })
 
     return depositions
   }
